d255bc5a9f6c6fa02ab5bab34a4b1805b8d93b91
HarshBundela <0harsh0000@gmail.com>
2025-03-29 09:32:19 +0530

    practice commit

---
+++ javaBasics.js
@@ -0,0 +1,15 @@
+console.log("Hello from harsh"); //print
+/*praintianteankifn*/ 
+var myName = "harsh";
+myName =8;
+let ourName = "freeCodeCmap";
+
+var a;
+console.log(a);
+var b =2;
+a=7.0*1.1;
+
+console.log(a);
+b=a;
+console.log(a);
+
